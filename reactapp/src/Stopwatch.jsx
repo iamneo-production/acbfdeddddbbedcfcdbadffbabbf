fbd3098f2693327897dff82a0b96a32c84a0244a
Iamneo <techadmin@iamneo.ai>
2023-08-01 10:28:05 +0000

    Backup Commit

--- reactapp/src/Stopwatch.jsx
+++ reactapp/src/Stopwatch.jsx
@@ -19,7 +19,7 @@ const Stopwatch = () => {
     let interval = null;
     if (isRunning) {
       interval = setInterval(() => {
-        setTime((time) => time + 1);
+        setTime((Time) => time + 1);
       }, 1000);
     } else if (!isRunning && time !== 0) {
       clearInterval(interval);
@@ -91,4 +91,4 @@ const Stopwatch = () => {
   );
 };
 
-export default Stopwatch;
+export default Stopwatch;
--- reactapp/src/tests/Stopwatch.test.js
+++ reactapp/src/tests/Stopwatch.test.js
@@ -7,7 +7,7 @@ describe('Testing Stopwatch', () => {
     jest.setTimeout(30000);
 
 
-    test('FE_before_start', () => {
+    test('testcase1', () => {
         
         act(() => {
             render(<Stopwatch />);
@@ -25,7 +25,7 @@ describe('Testing Stopwatch', () => {
 
     }, 30000);
 
-    test('FE_running_stopwatch', async () => {
+    test('testcase2', async () => {
 
         
         act(() => {
@@ -36,8 +36,7 @@ describe('Testing Stopwatch', () => {
         const reset = screen.getByTestId('reset');
 
         const time = screen.getByTestId('time');
-
-        fireEvent.click(start);
+fireEvent.click(start);
 
         expect(screen.queryByTestId('start')).toBeNull();
         expect(screen.queryByTestId('pause')).toBeInTheDocument();
@@ -62,8 +61,6 @@ describe('Testing Stopwatch', () => {
         })
 
         fireEvent.click(reset);
-        expect(time.textContent.split(' ').join('')).toBe('00:00:00');
-
-    })
+        expect(time.textContent.split(' ').join('')).toBe('00:00:00');})
 
 })
